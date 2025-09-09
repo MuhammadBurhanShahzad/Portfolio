3f6fdbf36d4f6f0c888ecd73d13fbdc83e79efbe
Muhammad Burhan Shahzad <burishahzada7728@gmail.com>
2025-09-09 11:12:20 +0500

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1VHkNMDhgdlOlD6J14GWj21PA5te4WOPn/view"
+            href="https://drive.google.com/file/d/19arRSh4dyDsZDcWQthmdJPOuvtMG08ra/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
