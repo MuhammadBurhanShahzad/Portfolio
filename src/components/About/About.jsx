a21235940ae157136494f3697b4dadef517a6bbf
Muhammad Burhan Shahzad <burishahzada7728@gmail.com>
2025-11-05 10:51:46 +0500

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/pfp.jpeg';
+import profileImage from '../../assets/pfp final.jpg';
 
 const About = () => {
   return (
