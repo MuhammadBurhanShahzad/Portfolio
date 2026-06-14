de430c1f470c3461b2da1dc2a52b85cd6f8fed2c
Muhammad Burhan Shahzad <burishahzada7728@gmail.com>
2026-06-14 11:15:36 +0500

    Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/Fair onw.png';
+import profileImage from '../../assets/final.png';
 
 const About = () => {
   return (
